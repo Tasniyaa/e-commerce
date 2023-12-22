5f2a0226d2cfe013dbda8320bf9db1c1a61d773f
Nishat-Tasniya <122631608+Nishat-Tasniya@users.noreply.github.com>
2023-12-22 19:28:25 +0600

    login

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import About from './pages/About';
 import Pagenotfound from './pages/Pagenotfound';
 import Policy from './pages/Policy';
 import Contact from './pages/Contact';
+import Register from './pages/Auth/Register';
+import Login from "./pages/Auth/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage/>} />
+        <Route path='/register' element={<Register/>} />
+       <Route path='/login' element={<Login/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/policy' element={<Policy/>} />
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 
 import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
 
 //config dotenv
 dotenv.config();
@@ -16,6 +17,7 @@ connectDB();
 const app = express();
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
